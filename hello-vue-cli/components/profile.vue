1bbc50f6f9fe80d8b5dd1c852c79aa78d2594725
hanneka <98503316+hanneka@users.noreply.github.com>
2022-03-23 08:24:21 +0700

    Add files via upload

---
+++ hello-vue-cli/components/profile.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="profile">
+        <h2>{{ msg }}</h2>
+
+        <ul>
+            <li><h2>This is the Profile Component</h2></li>
+            <li><h4>Objective:</h4></li>
+            <li><h4>Name: Hanneka Rose C. Puri</h4></li>
+            <li><h4>Course: Information Technology with Specialization in Web Development</h4></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'profile',
+    data () {
+        return {
+            msg: 'This is the Profile Component'
+        }
+    }
+}
---
+++ hello-vue-cli/components/skills.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="skills">
+        <h2>{{ msg }}</h2>
+
+        <ul>
+            <li><h2>This is the Skill Component</h2></li>
+            <li><h4>HTML</h4></li>
+            <li><h4>CSS</h4></li>
+            <li><h4>JavaScript</h4></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'skills',
+    data () {
+        return {
+            msg: 'This is the Skills Component'
+        }
+    }
+}
